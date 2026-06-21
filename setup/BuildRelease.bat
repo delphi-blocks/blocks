56ab1d1f749e95dd0e6ffd33905ec2ef6e1eb83b
Luca Minuti <luca.minuti@gmail.com>
2026-06-21 11:54:11 +0200

    Detect RAD Studio install path via registry and sign build outputs

--- setup/BuildRelease.bat
+++ setup/BuildRelease.bat
@@ -1,7 +1,20 @@
 @ECHO OFF
 
 :: Delphi 13.0 Florence
-@SET BDS=C:\Program Files (x86)\Embarcadero\Studio\37.0
+set "BDSVER=37.0"
+
+for /f "tokens=2,*" %%A in (
+    'reg query "HKCU\SOFTWARE\Embarcadero\BDS\%BDSVER%" /v RootDir 2^>nul'
+) do set "BDS=%%B"
+
+if not defined BDS (
+    echo ERROR: RAD Studio %BDSVER% not found in the registry.
+    exit /b 1
+)
+
+REM remove any trailing backslash
+if "%BDS:~-1%"=="\" set "BDS=%BDS:~0,-1%"
+
 CALL "%BDS%\bin\rsvars.bat"
 ::::::::::::::::::::::::::::::::
 
@@ -20,16 +33,30 @@ SET BUILDCONFIG="/p:config=%_CONFIG%"
 SET BUILDPLATFORM="/p:platform=%_PLATFORM%"
 SET EXEOUTPUT="/p:DCC_ExeOutput=..\\setup\\%_PLATFORM%"
 
+:: Platform name without quotes, used to build output paths below
+SET "PLATFORMDIR=%_PLATFORM:"=%"
+
+:: Script used to sign executables; called only if it exists
+SET "SIGNSCRIPT=%~dp0SignFile.bat"
+
 SET "ERRORCOUNT=0"
 
 @ECHO OFF
 :: Build BLOCKS.EXE
 msbuild ..\Source\Blocks.dproj /t:Rebuild %BUILDTARGET% %BUILDCONFIG% %BUILDPLATFORM% %EXEOUTPUT%
-IF %ERRORLEVEL% NEQ 0 set /a ERRORCOUNT+=1
+IF %ERRORLEVEL% NEQ 0 (
+  set /a ERRORCOUNT+=1
+) ELSE (
+  IF EXIST "%SIGNSCRIPT%" CALL "%SIGNSCRIPT%" "%~dp0%PLATFORMDIR%\Blocks.exe"
+)
 
 :: Build LAUNCHER.EXE
 msbuild ..\Source\Launcher.dproj /t:Rebuild %BUILDTARGET% %BUILDCONFIG% %BUILDPLATFORM% %EXEOUTPUT%
-IF %ERRORLEVEL% NEQ 0 set /a ERRORCOUNT+=1
+IF %ERRORLEVEL% NEQ 0 (
+  set /a ERRORCOUNT+=1
+) ELSE (
+  IF EXIST "%SIGNSCRIPT%" CALL "%SIGNSCRIPT%" "%~dp0%PLATFORMDIR%\Launcher.exe"
+)
 
 
 IF %ERRORCOUNT% NEQ 0 (
@@ -71,3 +98,15 @@ if "%INNO_PATH:~-1%"=="\" set "INNO_PATH=%INNO_PATH:~0,-1%"
 
 :: Run InnoSetup compiler
 "%INNO_PATH%\ISCC.exe" blocks.iss /DPlatform=%_PLATFORM%
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
+:: Find the installer just produced (most recently modified .exe in Output) and sign it
+IF EXIST "%SIGNSCRIPT%" (
+  SET "SETUPSIGNED="
+  FOR /F "delims=" %%F IN ('dir /b /o-d "%~dp0Output\*.exe"') DO (
+    IF NOT DEFINED SETUPSIGNED (
+      SET "SETUPSIGNED=1"
+      CALL "%SIGNSCRIPT%" "%~dp0Output\%%F"
+    )
+  )
+)
